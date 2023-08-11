cb0f258c080c4f2d5242f61fa0905b53f6187f5d
Paul Maxwell <maxwellpaulm@gmail.com>
2023-08-11 17:54:30 -0400

    Adding shortcuts

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -70,6 +70,7 @@ end
 alias clear='echo -en "\x1b[2J\x1b[1;1H" ; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo'
 
 # vim
+alias n='nvim .'
 alias vim='nvim'
 alias t='tmux new-session -A -s session'
 
